1e5937efa206eee9d9c4bd7a665430d3b90bc848
Brooks Newberry <mail@brooks.is>
2016-04-05 00:00:15 -0700

    Add messages to tests

--- tests/accessibilityObjects.js
+++ tests/accessibilityObjects.js
@@ -4,9 +4,11 @@ import access from '../src/actions/fetchAccessibilityObject.js'
 
 test('Fetch valid accessibility data', function(t){
   t.plan(3);
-  access(9999).catch(t.ok)
+  access(9999).catch(function(err){
+    t.ok(err, 'Expect non-existant ppid to be rejected.');
+  })
   access(2135).then(function(res){
-    t.ok(has(res, `2135.sec_1.qid_12`));
-    t.notOk(has(res['2135'], null));
+    t.ok(has(res, `2135.sec_1.qid_12`), 'Expect parking question twelve.');
+    t.notOk(has(res['2135'], null), 'No null sections.');
   })
 });
