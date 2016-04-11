06aa7d85c3c835aa3d1cafcd2b18706ff2596bee
Brooks Newberry <mail@brooks.is>
2016-04-11 15:16:06 -0700

    Finish paths story, closes #11

--- src/actions/stories/paths.js
+++ src/actions/stories/paths.js
@@ -24,9 +24,40 @@ var paths = function(pollingPlace) {
   
   //Steps warnings
   subCategories.forEach( (sub) => {
-    if (sub.qid_1.answer === 'No') pathsWithSteps.push('the path ' + sub.qid_0.answer);
+    if (sub.qid_1.answer === 'No' && sub.qid_2.answer !== 'Yes') pathsWithSteps.push('the path ' + sub.qid_0.answer);
   });
   if (pathsWithSteps.length > 0) response.warnings.push('There are stairs on ' + and(pathsWithSteps) + '.');
+  
+  //Warnings which only mention a single path, e.g. 'from Parking', and should be ordered by path.
+  subCategories.forEach( (sub) => {
+    let pathFrom = sub.qid_0.answer;
+    //Missing directional signs
+    if (sub.qid_12.answer === 'No') response.warnings.push(`The accessible route ${fromPath} is not the regular pedestrian route, but directional signs are not visible.`);
+    
+    //Missing ISA sign
+    if (sub.qid_3.answer === 'Yes') response.warnings.push(`The alternate accessible path ${pathFrom} is not marked by an ISA sign.`);
+    
+    //Narrow path
+    let narrowPathWidth = sub.qid_4.data ? ` (only ${sub.qid_4.data.replace('"', ' inches')})` : '';
+    if (sub.qid_4.answer === 'No') response.warnings.push(`The path ${pathFrom} is narrow${narrowPathWidth}.`);
+    
+    //Slippery path
+    if (sub.qid_5.answer === 'No') response.warnings.push(`The path ${pathFrom} is slippery or unstable.`);
+    
+    //Steep ramp
+    if (sub.qid_6.answer === 'No' || sub.qid_7.answer === 'No' || sub.qid_8.answer === 'No') {
+      response.warnings.push(`The path ${pathFrom} has a steep ramp.`);
+    }
+    
+    //Wide grates
+    if (sub.qid_9.answer === 'No') response.warnings.push(`The path ${pathFrom} follows a grate with spaces greater than ͑½ inch wide, where wheels may get stuck.`);
+    
+    //Overhead obstacles (sight-impairment)
+    if (sub.qid_10.answer === 'No') response.warnings.push(`The path ${pathFrom} has overhead obstacles that cannot be detected with a cane.`);
+    
+    //Wall-mounted protrusions
+    if (sub.qid_11.answer === 'No') response.warnings.push(`The path ${pathFrom} has wall-mounted protrusions greater than 4 inches.`);
+  });
   
   return response;
 };
--- tests/paths.js
+++ tests/paths.js
@@ -8,12 +8,12 @@ test('Path stories', function(t){
   .then(function(res){
     let story = paths(res['15']);
     t.equals(story.summary, 'There is a path from Parking and a path from Property line.', 'Correct paths summary for ppid 15');
-    t.equals(story.warnings.length, 0, 'Expects no paths warnings for ppid 15');
+    t.equals(story.warnings.length, 4, 'Expects no paths warnings for ppid 15');
   })
   access(3120)
   .then(function(res){
     let story = paths(res['3120']);
     t.equals(story.summary, 'There is a path from Parking and a path from Public Transportation.', 'Correct paths summary for ppid 3120');
-    t.equals(story.warnings.length, 1, 'Expects one path warning for ppid 3120');
+    t.equals(story.warnings.length, 3, 'Expects one path warning for ppid 3120');
   })
 });
