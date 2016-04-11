afb3da7284a357d0d5d7cad276d0a25968341e73
Hans Chun <hanschun@gmail.com>
2016-04-11 16:12:46 -0700

    Edit on About page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -41,16 +41,16 @@ export default class About extends React.Component {
         	<div style={styles.info}>
         		<h2 style={styles.headers}>Our Mission</h2>
         		<p>This app was created as a part of Hack4Sac's county innovation challenge.</p>
-        		<p>We believe that voters with specific needs are people too, and that they are often cast aside when creating 
+        		<p>We believe that voters with specific needs are people too, and that they are often cast aside when creating
         		products or services.</p>
-        		<p>That's why we created this app. Rather than design the app for beauty, we tried our best to design the app to be easy for
+          <p>That's why we created this app. We tried our best to design the app to be easy for
         		people with special needs to use. </p>
         		<p>One very helpful tool that we recommend all developers use is <a href="http://khan.github.io/tota11y/">Khan Academy's tota11y</a>.</p>
         	</div>
         	<div style={styles.info}>
         		<h2 style={styles.headers}>Giving Back</h2>
         		<p>We believe in our mission so much, we will put our money where our mouth is.</p>
-        		<p>If we win the Hack4Sac challenge, we will donate 20% of our winnings to the <a href="http://www.aapd.com/">American Association 
+        		<p>If we win the Hack4Sac challenge, we will donate 20% of our winnings to the <a href="http://www.aapd.com/">American Association
         		of People with Disabilities (AAPD)</a>.</p>
         		<p>If you'd like to donate now, please click <a href="https://aapd.kindful.com/?campaign=238334">here to donate to the AAPD</a></p>
         	</div>
