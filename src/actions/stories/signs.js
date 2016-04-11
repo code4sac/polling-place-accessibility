56a664224d1156c74b43ea897b1a08b6ebee84ee
Brooks Newberry <mail@brooks.is>
2016-04-11 11:18:13 -0700

    init signs story

---
+++ src/actions/stories/signs.js
@@ -0,0 +1,9 @@
+var and = require('join-component');
+
+var signs = function(pollingPlace) {
+  var response = { summary: 'There are signs.', warnings: [] };
+  
+  return response;
+};
+
+module.exports = signs;
