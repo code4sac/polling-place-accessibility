12a43f4ea4c8af76520e70112099241a6ddcfb9a
Hans Chun <hanschun@gmail.com>
2016-04-11 15:15:03 -0700

    Finishing work on this branch so it can be merged

--- src/components/PollingPlaceForm.jsx
+++ src/components/PollingPlaceForm.jsx
@@ -7,11 +7,7 @@ import preFetchedPollingPlaces from '../map/preFetchedPollingPlaces.js'
 
 export default class PollingPlaceForm extends Component {
   constructor(props) {
-    // Junar API key: 365e06cd12a419135ae87d9f0ec0a8e60b25fbe3#sthash.qxY4OVZH
-    // Google API key: AIzaSyDRoMwLIG_AcxMeha5PIv9lWnM0AwWRsCM
-    // Fusion Table key: 1Wps1_Vj4dkNiAIozL47QINAYAhonMgfVf0F3aPyR
-    // https://www.googleapis.com/fusiontables/v2/query?sql=SELECT%20*%20FROM%201Wps1_Vj4dkNiAIozL47QINAYAhonMgfVf0F3aPyR&key=AIzaSyDRoMwLIG_AcxMeha5PIv9lWnM0AwWRsCM
-    super(props);
+    super(props)
     this.state = {
       userPpid: null,
       house: null,
