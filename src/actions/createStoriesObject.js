6d39b779bdfaf0beca4f9438b901c0703cbcd1e8
Brooks Newberry <mail@brooks.is>
2016-04-11 11:23:46 -0700

    Restrooms is ready to join the AccessibilityStory component accordion

--- src/actions/createStoriesObject.js
+++ src/actions/createStoriesObject.js
@@ -1,8 +1,10 @@
 import parking from './stories/parking.js'
+import restrooms from './stories/restrooms.js'
 
 var createStoriesObject = function(pollingPlace) {
   var result = {};
   result.Parking = parking(pollingPlace);
+  result.Restrooms = restrooms(pollingPlace);
   result.Info = {
     ppid: pollingPlace['Polling Place Information']['1-Poll-Place-Information']['qid_1'].answer,
     name: pollingPlace['Polling Place Information']['1-Poll-Place-Information']['qid_2'].answer,
