a4ce1dbf0191ce895c84adb65b1193f38d09f6cf
Brooks Newberry <mail@brooks.is>
2016-04-11 17:28:11 -0700

    restore components from master branch

---
+++ src/components/App.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import PollingPlaceForm from './PollingPlaceForm'
+import PlaceMap from './PlaceMap'
+import _ from 'lodash'
+import FormMap from './FormMap'
+import AccessibilityStory from './AccessibilityStory'
+import Resources from './Resources'
+import About from './About'
+require('./../lib/tota11y.min.js')
+
+const sacImage = require('./../assets/images/sacCapitol.jpg')
+
+export class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			currentTab: 'map'
+		}
+	}
+
+	getStyles() {
+		return {
+			root: {
+				height: '100%',
+				fontFamily: '"Helvetica Neue", Helvetica, Roboto, Arial, sans-serif',
+			},
+			header: {
+				width: '100%',
+				height: 300,
+				display: 'flex',
+				justifyContent: 'space-around',
+				alignItems: 'center',
+				textTransform: 'uppercase',
+				fontWeight: 300,
+				fontSize: 80,
+				textShadow: '2px 2px #333',
+				color: '#949494',
+				backgroundImage: `url("https://raw.githubusercontent.com/abustamam/polling-place-accessibility/master/public/images/sacCapitol.jpg")`,
+				backgroundSize: '100% 100%',
+    			backgroundRepeat: 'no-repeat',
+			},
+			tabs: {
+				width: '100%',
+				height: 50,
+				display: 'flex',
+				justifyContent: 'space-around',
+				alignItems: 'center',
+			},
+			tab: (tab) => {
+				return {
+					cursor: 'pointer',
+					background: tab === this.state.currentTab ? 'white' : 'rgba(1, 1, 1, .25)',
+					boxShadow: tab === this.state.currentTab ? '' : '0px -2px 5px 0px #777 inset',
+					borderTop: '1px solid black',
+					borderLeft: '.5px solid black',
+					borderRight: '.5px solid black',
+					display: 'flex',
+					flexGrow: 1,
+					justifyContent: 'space-around',
+					height: '100%',
+					alignItems: 'center',
+				}
+			},
+			currentTab: {
+				height: 'calc( 100vh - 300px - 50px)'
+			}
+		}
+	}
+
+	render() {
+
+		const tabs = ['map', 'find your polling place', 'resources', 'about']
+		const styles = this.getStyles()
+
+		let renderedTab = (tab) => {
+			if (tab === 'map') {
+				return <PlaceMap />
+			} else if (tab === 'find your polling place') {
+				return <div>
+					<PollingPlaceForm approxLat={this.props.approxLat} approxLong={this.props.approxLong} />
+			        <FormMap approxLat={this.props.approxLat} approxLong={this.props.approxLong} />
+			        <AccessibilityStory />
+				</div>
+			} else if (tab === 'resources') {
+				return <Resources/>
+			} else if (tab === 'about') {
+				return <About/>
+			}
+		}
+
+		return (
+			<div style={styles.root}>
+				<div style={styles.header} role="header">
+					Poll Locations
+				</div>
+				<div style={styles.tabs} role="navigation">
+					{_.map(tabs, tab => {
+						return <div key={tab} style={styles.tab(tab)} onClick={()=>this.setState({currentTab: tab})}>
+							<span>{_.capitalize(tab)}</span>
+						</div>
+					})}
+				</div>
+				<div style={styles.currentTab} role="main">
+					{ renderedTab(this.state.currentTab) }
+				</div>
+	  		</div>
+		);
+	}
+}
+
+App.defaultProps = {
+  approxLat: '38.5789777', //geocoding bias
+  approxLong: '-121.4829292', //geocoding bias
+};
---
+++ src/components/PollingPlaceForm.jsx
@@ -0,0 +1,146 @@
+import React, {Component} from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import pollingPlaceRequests from '../actions/pollingPlaceRequests';
+import _ from 'lodash'
+import store from '../stores/pollStore.js';
+import preFetchedPollingPlaces from '../map/preFetchedPollingPlaces.js'
+
+export default class PollingPlaceForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userPpid: null,
+      house: null,
+      zip: null,
+      dob: null,
+      place: null,
+      address: null
+    };
+  }
+
+  getStyles() {
+    return {
+      root: {
+
+      },
+      form: {
+        display: 'flex',
+        flexDirection: 'column'
+      },
+      header: {
+        fontSize: 40,
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+        textAlign: 'center'
+      },
+      form: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 10,
+      },
+      formGroup: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: 10,
+        minHeight: 50
+      },
+      formInput: {
+        height: '2.4375rem',
+        fontSize: '1rem'
+      }
+    }
+  }
+
+  render() {
+    const styles = this.getStyles()
+    var caption = this.state.caption || this.props.caption;
+    var addressLink = this.state.address ? <a href={'http://maps.google.com/?q='+encodeURI(this.state.address)}>➶</a> : null;
+
+    return (
+      <div className="polling-place-form-row row">
+        <div className="medium-12 columns" style={styles.form}>
+          <div>
+            <h1 style={styles.header}>Polling Place Form</h1>
+          </div>
+          <form id="polling-place-form" data-abide role="form">
+            <div style={styles.form}>
+              <div style={styles.formGroup}>
+                <label htmlFor="house">House Number</label>
+                <input style={styles.formInput} type="number" name="house" id="house" placeholder="e.g. '1234'" required onChange={this.handleHouseChange.bind(this)}/>
+              </div>
+              <div style={styles.formGroup}>
+                <label htmlFor="zip">5-Digit Zip Code</label>
+                <input style={styles.formInput} type="number" name="zip" id="zip" placeholder="e.g. '54321'" required onChange={this.handleZipChange.bind(this)}/>
+              </div>
+              <div style={styles.formGroup}>
+                <label htmlFor="dob">Date of Birth <small>(MM/DD/YYYY)</small></label>
+                <input style={styles.formInput} type="date" placeholder="MM/DD/YYYY" name="dob" id="dob" required onChange={this.handleDobChange.bind(this)}/>
+              </div>
+              <div style={styles.formGroup}>
+                <button style={styles.formInput} onClick = {(e) => this.handleSubmit(e)}>Submit</button>
+              </div>
+            </div>
+          </form>
+          <p>{caption} {addressLink}</p>
+        </div>
+      </div>
+    );
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    pollingPlaceRequests.voter(this.state.house, this.state.zip, this.state.dob, this.props.fusionkey)
+    .then((data) =>{
+      if (data.MailDate && data.MailDate != '') {
+        this.setState({caption: `Your absentee ballot was mailed to this address on ${data.MailDate}. You may still vote at your polling place on election day.`, mailDate: data.MailDate})
+      }
+      return data.PollName;
+    })
+    .then((pollid) =>{
+      this.setState({userPpid: pollid});
+      store.setPPID(pollid);
+      return pollingPlaceRequests.place(pollid, this.props.fusionkey);
+    })
+    .then((place) => {
+      store.setVals({ppName: place['Polling place'], ppAddress: `${place.Address} ${place.City}, ${this.props.stateAbbr} ${place.Zip}`, ppLat: place.Lat, ppLong: place.Long});
+      this.setState({place: place['Polling place'], address: `${place.Address} ${place.City}, ${this.props.stateAbbr} ${place.Zip}`, latitude: place.Lat, longitude: place.Long});
+      var caption = '';
+      if (this.state.mailDate && this.state.mailDate != '') caption += `Your absentee ballot was mailed to this address on ${this.state.mailDate}. You may still vote at your polling place on election day. `;
+      caption += `Your polling place is located at ${place['Polling place']}.`;
+      this.setState({caption});
+      if (!place.Lat || !place.Long) {
+        pollingPlaceRequests.geocode(this.state.address, this.props.mapboxkey, this.props.approxLat, this.props.approxLong)
+        .then((result) =>{
+          this.setState({latitude: result.center[1], longitude: result.center[0]});
+        });
+      }
+    })
+    .catch((err) => {
+      this.setState({caption: `There was an error finding your polling place.`});
+      this.setState({place: null, address: null, latitude: null, longitude: null, userPpid: null});
+    });
+  }
+  handleHouseChange(e) {
+    this.setState({house: e.target.value});
+  }
+  handleZipChange(e) {
+    this.setState({zip: e.target.value});
+  }
+  handleDobChange(e) {
+    this.setState({dob: e.target.value});
+  }
+}
+
+PollingPlaceForm.defaultProps = {
+  fusionkey: 'AIzaSyDRoMwLIG_AcxMeha5PIv9lWnM0AwWRsCM', //read-only access
+  mapboxkey: 'pk.eyJ1IjoiYnJvb2tzbiIsImEiOiJjaWpkbmkzMDEwMDh3dGdra2Y0OHYwbjViIn0.gqY3_NGpI96FuDQ7csaOUw', //geocoding API
+  caption: 'Enter your house number, zip code, and date of birth to find your polling place.',
+  stateAbbr: 'CA',
+  styles: {
+    root: {
+      height: '10em'
+    },
+    map: {
+      height: '100%'
+    }
+  }
+};
